2e86637029802602731690401a553981bf6db569
cancanneed <109750698+hkzxk@users.noreply.github.com>
2023-03-29 08:08:46 +0800

    Add files via upload

---
+++ assignment_4/clock.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate_1
+{
+    public class clock
+    {
+        static string set_time;
+        private string time;
+        public delegate void TickHandler(Object sender, TickEventArgs e);
+        public event TickHandler Show_time;
+
+        
+        public class TickEventArgs : EventArgs
+        {
+            public readonly string Time;
+            public TickEventArgs(string time)
+            {
+                Time = time;
+            }
+        }
+        protected virtual void Alarm(TickEventArgs e)
+        {
+            if(Show_time != null)
+            {
+                Show_time(this, e);
+            }
+        }
+
+        public void Tick()
+        {
+            while (true)
+            {
+                time = string.Format("{0:yyyy-MM-dd HH:mm:ss}", DateTime.Now);
+                //TickEventArgs e = new TickEventArgs(time);
+                //Alarm(e);
+
+                if (time == set_time)
+                {
+                     TickEventArgs e = new TickEventArgs(time);
+                    Alarm(e);
+                }
+            }
+        }
+
+        public class Shower
+        {
+            public void time(Object sender,clock.TickEventArgs e) {
+
+                clock c = (clock)sender;
+                Console.WriteLine("Time is {0} now. ", e.Time);
+                Console.WriteLine();
+
+
+            }
+        }
+
+        public class Alarmer
+        {
+            
+            public static void Alarming(Object sender, clock.TickEventArgs e)
+            {
+                clock c = (clock)sender;
+                for (int a = 0; a < 60; a++)
+                    Console.WriteLine("Alarming!! Alarming!!");
+                Console.WriteLine();
+            }
+
+
+        }
+        public class Program
+        {
+            static void Main() {
+                clock c = new clock();
+                Shower s = new Shower();
+                c.Show_time += s.time;
+                c.Show_time += (new Shower()).time;
+                c.Show_time += Alarmer.Alarming;
+
+                c.Tick();
+
+            }
+
+        }
+
+
+    }
+}
---
+++ assignment_4/generic.cs
@@ -0,0 +1,92 @@
+﻿
+public class Node<T> where T:IComparable<T>//此处实现了IComparable接口
+{
+    public Node<T> Next { get; set; }
+    public T Data { get; set; }
+    public Node(T t)
+    {
+        Data = t;
+        Next = null;
+        
+    }
+    //定义一个函数用于比较泛型节点大小
+    public bool Compare(Node<T> node)
+    {
+
+        if (Data.GetType() != node.Data.GetType()) throw new ImcomparableException("The elements are not comparable!");
+        if (Data.CompareTo(node.Data) < 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+        
+    
+}
+//异常定义
+public class ImcomparableException : Exception
+{
+    public ImcomparableException(string message): base(message)
+    {
+        Console.WriteLine(message);
+    }
+} 
+public class EmptyLinkListException : Exception
+{
+    public EmptyLinkListException(string message) : base(message) { 
+        Console.WriteLine(message);
+    }
+}
+
+//链表类
+public class LinkList<T> where T:IComparable<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+    
+    public LinkList()
+    {
+        head = tail = null;
+    }
+    public Node<T> Insert(T t)
+    {
+        Node<T> n = new Node<T>(t);
+
+        n.Next = head.Next;
+        head.Next = n;
+        return head;
+    }
+    //输出所有节点
+    public void Print()
+    {
+        Node<T> tmp = head;
+        int inx = 1;
+        while(tmp != null)
+        {
+            Console.WriteLine("the element{0} is {1}",inx, tmp.Data);
+            inx++;
+        }
+    }
+    //求最大的节点
+    public Node<T> Max() 
+    {
+        Node<T> tmp = head.Next;
+        if(tmp == null)
+        {
+            Exception e = new EmptyLinkListException("The linklist is empty!");
+            throw e;
+        }
+        Node<T> max_node = tmp;
+        while (tmp != null)
+        {
+            
+            if (max_node.Compare(tmp))
+            {
+                max_node = tmp;
+                tmp = tmp.Next;
+            }
+            
+        }
+        return max_node;
+    }
+}
